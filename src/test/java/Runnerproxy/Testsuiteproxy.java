4e0a4082a86dc14b25429e456ef01e5d684b0e36
aloht1998 <aloht1998@gmail.com>
2021-06-03 11:16:12 +0700

    push src/

---
+++ src/test/java/Runnerproxy/Testsuiteproxy.java
@@ -0,0 +1,72 @@
+package Runnerproxy;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class Testsuiteproxy {
+
+        WebDriver Object;
+
+        public void login(String username, String password) {
+            WebElement txtUsername = Object.findElement(By.xpath("//input[@id='user-name']"));
+            WebElement txtPassword = Object.findElement(By.xpath("//input[@id='password']"));
+            WebElement btnLogin = Object.findElement(By.xpath("//input[@id='login-button']"));
+            txtUsername.sendKeys(username);
+            txtPassword.sendKeys(password);
+            btnLogin.click();
+        }
+
+        @Before
+        public void Setup() {
+            String s;
+            s = System.setProperty("webdriver.gecko.driver", "C:\\Webdriver\\geckodriver.exe");
+            Object = new FirefoxDriver();
+            Object.get("https://www.saucedemo.com/");
+            Object.manage().window().maximize();
+        }
+
+        @Test
+        public void Login_Thanh_Cong() {
+            login("standard_user", "secret_sauce");
+            Object.navigate().to("https://www.saucedemo.com/inventory.html");
+        }
+
+        @Test
+        public void Login_voi_username_sai() {
+            login("wrong_user", "secret_sauce");
+            WebElement errorLabel = Object.findElement(By.xpath("//div[@class='login-box']//h3"));
+            Assert.assertEquals("Epic sadface: User name khong hop le"
+                    , errorLabel.getText());
+        }
+
+        @Test
+        public void Login_voi_password_sai() {
+            login("standard_user", "wrong_sauce");
+            WebElement errorLabel = Object.findElement(By.xpath("//div[@class='error-message-container error']//h3"));
+            Assert.assertEquals("Epic sadface: Username and password do not match any user in this service"
+                    , errorLabel.getText());
+        }
+        @Test
+
+        public void Login_voi_usename_trong() {
+            login("",
+                    "secret_sauce");
+            WebElement erroLaberl = Object.findElement(By.xpath("//div[@class='error-message-container error']//h3"));
+            Assert.assertEquals("Epic sadface: Username is required"
+                    , erroLaberl.getText());
+        }
+        @After
+
+        public void tearDown() {
+            Object.close();
+        }
+
+
+}
